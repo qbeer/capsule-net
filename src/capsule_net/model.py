37452f9a5bf8c8d0df6b0aa75aaea34900a845c1
Alex Olar <olaralex666@gmail.com>
2020-09-26 15:51:56 +0200

    capsule net initial commit

---
+++ src/capsule_net/model.py
@@ -0,0 +1,93 @@
+from numpy.core.fromnumeric import repeat, transpose
+import torch as T
+import torch.nn.functional as F
+from torch.nn import Conv2d, Linear
+
+import numpy as np
+from torch.nn.modules import transformer
+
+
+class CapsuleNetwork(T.nn.Module):
+    def __init__(self, batch_size, capsule_dims=[8, 16], n_caps=[1152, 10]) -> None:
+        super(CapsuleNetwork, self).__init__()
+        self.capsule_dims = capsule_dims
+        self.n_caps = n_caps
+        self.conv1 = Conv2d(in_channels=3,
+                            out_channels=256,
+                            kernel_size=9,
+                            stride=1)
+        self.conv2 = Conv2d(in_channels=256,
+                            out_channels=256,
+                            kernel_size=9,
+                            stride=2)
+
+        stddev = .01
+        self.transformation_weights = T.nn.Parameter(T.normal(
+            mean=0,
+            std=stddev,
+            size=(1, n_caps[0], n_caps[1], capsule_dims[1], capsule_dims[0])),
+                                                     requires_grad=True)
+
+        self.raw_weights = T.nn.Parameter(
+            T.zeros(size=(batch_size, self.n_caps[0], self.n_caps[1], 1, 1)))
+
+    def _squash(self, tensor, axis=-1, epsilon=1e-8):
+        norm = self._norm(tensor, axis=axis, keepdims=True)
+        squash = T.square(norm) / (1. + T.square(norm))
+        unit = tensor / norm
+        return unit * squash
+
+    def _norm(self, tensor, axis=-1, keepdims=False, epsilon=1e-8):
+        squared_norm = T.sum(T.square(tensor), axis=axis, keepdims=keepdims)
+        return T.sqrt(squared_norm + epsilon)
+
+    def forward(self, inputs):
+        x, y = inputs
+
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        batch_size, channels, width, heigth = x.shape
+        x = T.reshape(x, (batch_size, width * heigth * channels //
+                          self.capsule_dims[0], self.capsule_dims[0]))
+        x = self._squash(x)
+
+        x = x.view(batch_size, self.n_caps[0], 1, self.capsule_dims[0],
+                   1).repeat(1, 1, self.n_caps[1], 1, 1)
+
+        tiled_transformation_weights = self.transformation_weights.repeat(
+            batch_size, 1, 1, 1, 1)
+
+        routing_weights = F.softmax(self.raw_weights, dim=2)
+
+        caps2 = T.matmul(tiled_transformation_weights, x)
+        weighted_preds = T.mul(routing_weights, caps2)
+        weighted_sum = T.sum(weighted_preds, axis=1, keepdims=True)
+        caps2_round1 = self._squash(weighted_sum, axis=-2)
+
+        caps2_round1_tiled = caps2_round1.repeat(1, self.n_caps[0], 1, 1, 1)
+
+        agreement = T.matmul(caps2.transpose(3, 4), caps2_round1_tiled)
+        raw_weights_2 = T.add(self.raw_weights, agreement)
+        routing_weights = F.softmax(raw_weights_2, dim=2)
+
+        weighted_preds = T.mul(routing_weights, caps2)
+        weighted_sum = T.sum(weighted_preds, axis=1, keepdims=True)
+        caps2_round2 = self._squash(weighted_sum, axis=-2)
+
+        caps2_round2_tiled = caps2_round2.repeat(1, self.n_caps[0], 1, 1, 1)
+
+        agreement = T.matmul(caps2.transpose(3, 4), caps2_round2_tiled)
+        raw_weights_3 = T.add(self.raw_weights, agreement)
+        routing_weights = F.softmax(raw_weights_3, dim=2)
+
+        weighted_preds = T.mul(routing_weights, caps2)
+        weighted_sum = T.sum(weighted_preds, axis=1, keepdims=True)
+        caps2_round3 = self._squash(weighted_sum, axis=-2)
+
+        caps2 = caps2_round3.squeeze()
+        
+
+        caps2_normed = self._norm(caps2)
+        preds = T.argmax(caps2_normed, axis=-1).squeeze()
+
+        return preds, caps2
---
+++ tests/test_model.py
@@ -0,0 +1,46 @@
+import unittest
+
+import numpy as np
+import torch
+from src.capsule_net.model import CapsuleNetwork
+
+class CapsuleNetTester(unittest.TestCase):
+    def get_random_inputs(self):
+        x = np.random.randn(self.batch_size, 3, 28, 28)
+        x = torch.from_numpy(x.astype(np.float32)).to(self.device)
+        y = np.random.randint(low=0, high=self.n_caps[-1], size=(self.batch_size,))
+        y = torch.from_numpy(y.astype(np.int32)).to(self.device)
+
+        return x, y
+
+    def setUp(self):
+        self.capsule_dims = [8, 16]
+        self.n_caps = [1152, 10]
+        self.batch_size = 32
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.capsule_net = CapsuleNetwork(batch_size=self.batch_size, capsule_dims=self.capsule_dims, n_caps=self.n_caps)
+        self.capsule_net = self.capsule_net.to(device=self.device)
+    
+    def test_ouput_prediction_shape_for_test_input(self):
+        inputs = self.get_random_inputs()
+        preds, capsules = self.capsule_net(inputs)
+        self.assertEqual(preds.shape, inputs[1].shape)
+        self.assertEqual(capsules.shape, (self.batch_size, self.n_caps[-1], self.capsule_dims[-1]))
+
+    def test_capsule_transformation_matrices(self):
+        weight_matrices = self.capsule_net.transformation_weights.detach().squeeze().cpu().numpy()
+        self.assertEqual(weight_matrices.shape, (self.n_caps[0], self.n_caps[1], self.capsule_dims[1], self.capsule_dims[0]))
+
+    def test_norm(self):
+        x, _ = self.get_random_inputs()
+        x_normed = self.capsule_net._norm(x, axis=1, keepdims=False)
+        self.assertEqual(x_normed.shape, (self.batch_size, 28, 28))
+        x_normed = self.capsule_net._norm(x_normed, axis=0, keepdims=True)
+        self.assertEqual(x_normed.shape, (1, 28, 28))
+
+    def test_squash(self):
+        x, _ = self.get_random_inputs()
+        x_squashed = self.capsule_net._squash(x, axis=1)
+        self.assertEqual(x_squashed.shape, (self.batch_size, 3, 28, 28))
+        norm_x = torch.norm(x, p=2, dim=1, keepdim=True).detach().cpu().numpy()
+        self.assertTrue((norm_x >= x_squashed.detach().cpu().numpy()).all())
