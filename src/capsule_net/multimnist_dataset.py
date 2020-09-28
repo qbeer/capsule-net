80310dcb2bf9ac0509e5e3e0a338d97a87a1d782
Alex Olar <olaralex666@gmail.com>
2020-09-28 15:47:25 +0200

    made multimnist deterministic option, created test to validate transforms

--- src/capsule_net/multimnist_dataset.py
+++ src/capsule_net/multimnist_dataset.py
@@ -1,22 +1,32 @@
+from PIL import Image
+import numpy as np
 import torch
 import torchvision
 import random
-import numpy as np
-from PIL import Image
 
 
 class MultiMnist(torch.utils.data.Dataset):
-    def __init__(self, transforms=None, transfrom_target=None,
-                 image_transforms=torchvision.transforms.Compose(transforms=[
-                     torchvision.transforms.RandomAffine(degrees=(-5, 5),
-                                                         translate=(.2, .2),
-                                                         scale=(.5, .7)),
-                     torchvision.transforms.RandomCrop(size=(26, 26)),
-                     torchvision.transforms.Resize(size=(28, 28))
-                 ]), train=True):
+    def __init__(self, transforms=None, target_transform=None,
+                 image_transforms=None, train=True, deterministic=False):
+        self.deterministic = deterministic
+        if self.deterministic:
+            self.seed = 42
+            torch.backends.cudnn.deterministic = True
+            torch.backends.cudnn.benchmark = False
+            torch.manual_seed(self.seed)
+            torch.cuda.manual_seed_all(self.seed)
+            np.random.seed(self.seed)
+            random.seed(self.seed)
+
         self.transforms = transforms
-        self.transform_target = transfrom_target
-        self.image_transforms = image_transforms
+        self.target_transform = target_transform
+        self.image_transforms = torchvision.transforms.Compose(transforms=[
+            torchvision.transforms.RandomAffine(degrees=(-5, 5),
+                                                translate=(.2, .2),
+                                                scale=(.5, .7)),
+            torchvision.transforms.RandomCrop(size=(26, 26)),
+            torchvision.transforms.Resize(size=(28, 28))
+        ])
         self.train = train
         self.mnist = torchvision.datasets.MNIST(
             root='/tmp/', download=True, train=self.train)
@@ -31,10 +41,14 @@ class MultiMnist(torch.utils.data.Dataset):
         image1, label1 = self.mnist[index1]
         image2, label2 = self.mnist[index2]
 
+        if self.deterministic:
+            random.seed(self.seed)
         while label1 == label2:
             index2 = random.randint(0, len(self))
             image2, label2 = self.mnist[index2]
 
+        if self.deterministic:
+            random.seed(self.seed)
         if self.image_transforms:
             image1 = self.image_transforms(image1)
             image2 = self.image_transforms(image2)
@@ -44,14 +58,18 @@ class MultiMnist(torch.utils.data.Dataset):
         blend = np.where(x > y, x, y)
         image = Image.fromarray(blend)
 
+        if self.deterministic:
+            random.seed(self.seed)
         if self.transforms:
             image = self.transforms(image)
 
-        label = np.zeros(shape=(10, ))
-        label[label1] = 1
-        label[label2] = 1
+        label = np.zeros(shape=(1, 10), dtype=np.uint8)
+        label[:, label1] = 1
+        label[:, label2] = 1
 
-        if self.transform_target:
-            label = self.transform_target(label)
+        if self.deterministic:
+            random.seed(self.seed)
+        if self.target_transform:
+            label = self.target_transform(label)
 
         return image, label
--- tests/test_multimnist_dataset.py
+++ tests/test_multimnist_dataset.py
@@ -1,18 +1,38 @@
-import unittest
-from src.capsule_net.multimnist_dataset import MultiMnist
-import numpy as np
+import torchvision
 import matplotlib.pyplot as plt
+import numpy as np
+from src.capsule_net.multimnist_dataset import MultiMnist
+import unittest
+import torch
 
 
 class MultiMnistDatasetTests(unittest.TestCase):
     def setUp(self):
-        self.dataset = MultiMnist()
+        self.dataset = MultiMnist(deterministic=True)
 
     def test_output_shape_after_overlay(self):
         image, label = self.dataset[0]
         self.assertTrue(hasattr(image, "im"))
         self.assertEqual(image.size, (28, 28))
-        self.assertEqual(label.shape, (10,))
+        self.assertEqual(label.shape, (1, 10))
+
+    def test_transforms(self):
+        transforms = torchvision.transforms.RandomResizedCrop(size=(28, 28))
+        dataset = MultiMnist(transforms=transforms, deterministic=True)
+        image_tr, label_tr = dataset[0]
+        image, label = self.dataset[0]
+        self.assertListEqual(label_tr.tolist(), label.tolist())
+        self.assertListEqual(np.array(image_tr).tolist(),
+                             np.array(transforms(image)).tolist())
+
+    def test_to_tensor_image_and_target_transforms(self):
+        transform = torchvision.transforms.ToTensor()
+        target_transform = torchvision.transforms.ToTensor()
+        dataset = MultiMnist(transforms=transform,
+                             target_transform=target_transform)
+        image, label = dataset[0]
+        self.assertTrue(isinstance(image, torch.Tensor))
+        self.assertTrue(isinstance(label, torch.Tensor))
 
     def test_image_visualization(self):
         fig, axes = plt.subplots(
