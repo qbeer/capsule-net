88780385b16f34faec41d9d08d985fac6367e8c9
Alex Olar <olaralex666@gmail.com>
2020-10-01 09:43:45 +0200

    updated loss

--- src/capsule_net/loss.py
+++ src/capsule_net/loss.py
@@ -1,22 +1,17 @@
 import torch
 
 
-def margin_loss(outputs, labels, n_classes, dtype=torch.float32):
-    margin_positive = torch.Tensor([.9])
-    margin_negative = torch.Tensor([.1])
-    lambd = torch.Tensor([.5])
+def margin_loss(outputs, one_hot_labels):
+    margin_positive = .9
+    margin_negative = .1
+    lambd = .5
 
-    batch_size = len(labels)
-    one_hot_labels = torch.zeros(size=(batch_size, n_classes), dtype=dtype)
-    for i, label in enumerate(labels):
-        label = torch.LongTensor(label)
-        one_hot_labels[i] = one_hot_labels[i].scatter_(dim=0,
-                                                       index=label, value=1.)
+    batch_size = len(one_hot_labels)
 
-    postive_loss = torch.square(torch.max(torch.zeros_like(outputs),
-                                          margin_positive - outputs))
-    negative_loss = torch.square(torch.max(torch.zeros_like(outputs),
-                                           outputs - margin_negative))
+    postive_loss = torch.square(
+        torch.max(torch.zeros_like(outputs), margin_positive - outputs))
+    negative_loss = torch.square(
+        torch.max(torch.zeros_like(outputs), outputs - margin_negative))
 
     negative_loss = torch.mul((1. - one_hot_labels), negative_loss)
     postive_loss = torch.mul(one_hot_labels, postive_loss)
--- tests/test_loss.py
+++ tests/test_loss.py
@@ -1,22 +1,25 @@
 import unittest
-from src.capsule_net.loss import margin_loss
-import torch
+
 import numpy as np
+import torch
+from src.capsule_net.loss import margin_loss
 
 
 class TestLoss(unittest.TestCase):
     def setUp(self):
-        self.labels = [[1, 2], [0, 1, 2]]
-        self.n_classes = 3
+        self.labels = torch.tensor([[0, 1, 1], [1, 1, 1]], dtype=torch.float32)
 
-        outputs = np.array([
-            [[3.4, 4.3], [1.2, 3.2], [2.1, 2.1]],
-            [[3.2, 6.5], [3.3, 1.2], [2.1, 2.5]]], dtype=np.float32)
+        outputs = np.array([[[3.4, 4.3], [1.2, 3.2], [2.1, 2.1]],
+                            [[3.2, 6.5], [3.3, 1.2], [2.1, 2.5]]],
+                           dtype=np.float32)
         outputs = torch.from_numpy(outputs)
 
         self.outputs = torch.norm(outputs, p=2, dim=-1, keepdim=False)
 
     def test_loss_with_normed_outputs(self):
-        loss = margin_loss(self.outputs, self.labels, n_classes=self.n_classes)
+        loss = margin_loss(
+            outputs=self.outputs,
+            one_hot_labels=self.labels,
+        )
         self.assertIsNotNone(loss)
         self.assertEqual(loss.shape, ())
